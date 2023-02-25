e9780c590eb7e7009ebdd8c10437769a958698f0
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 04:58:35 +0200

    squash!

--- dist/config/pm2.js
+++ dist/config/pm2.js
@@ -1,2 +1,1 @@
-var t={apps:[{name:"discord",script:"./dist/apps/discord.js",watch:!0,autorestart:!0,force:!0}]};export { t as default };
-
+var t={apps:[{name:"discord",script:"./dist/apps/discord.js",watch:!0,autorestart:!0,force:!0}]};export{t as default};
--- dist/lib/env.js
+++ dist/lib/env.js
@@ -1,4 +1,1 @@
-import * as e from "dotenv";
-import { z as t } from "zod";
-e.config();var D=t.object({DISCORD_APPLICATION_ID:t.string().default(""),DISCORD_CLIENT_ID:t.string().default(""),DISCORD_CLIENT_SECRET:t.string().default(""),DISCORD_PUBLIC_KEY:t.string().default(""),DISCORD_TOKENS:t.string().default("")}).parse(process.env);export { D as default };
-
+import*as e from"dotenv";import{z as t}from"zod";e.config();var D=t.object({DISCORD_APPLICATION_ID:t.string().default(""),DISCORD_CLIENT_ID:t.string().default(""),DISCORD_CLIENT_SECRET:t.string().default(""),DISCORD_PUBLIC_KEY:t.string().default(""),DISCORD_TOKENS:t.string().default("")}).parse(process.env);export{D as default};
