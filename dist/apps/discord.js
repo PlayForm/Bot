24cfd80b694c8473ed5b6d993ea3d658931ef52e
Nikola Hristov <nikola@nikolahristov.tech>
2023-02-25 03:39:23 +0200

    squash!

--- dist/apps/discord.js
+++ dist/apps/discord.js
@@ -1,1 +1,3 @@
-import{Client as r}from"discord.js";import s from"../options/discord.js";for(const{token:t,options:o,events:e}of s){if(!o)continue;const n=new r(o);for(const[i,f]of e??[])n.on(i,f);n.login(t)}
+import { Client as r } from "discord.js";
+import s from "../options/discord.js";
+for(const{token:t,options:o,events:e}of s){if(!o)continue;const n=new r(o);for(const[i,f]of e??[])n.on(i,f);n.login(t)}
