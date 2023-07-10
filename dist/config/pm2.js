f902c11c51523f326ddb9ee2b78aff45bcc0901f
Nikola Hristov <nikola@nikolahristov.tech>
2023-07-10 03:18:44 +0300

    squash!

--- dist/config/pm2.js
+++
@@ -1,1 +0,0 @@
-var t={apps:[{name:"discord",script:"./dist/apps/discord.js",watch:!0,autorestart:!0,force:!0}]};export{t as default};
--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    discord: {};
-};
-export default _default;
